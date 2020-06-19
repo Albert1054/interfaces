752a007e72f64e8f6d467360fd2895d76af56b23
Alberto Cavalcanti <cavalcanti_alberto@yahoo.com.br>
2020-06-19 18:59:01 -0300

    inicio de projeto com intefaces e services

---
+++ InterfaceServices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
